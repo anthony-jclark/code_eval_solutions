d3585ecc999dc7c116e14f5a30b247b2107dce08
Seth <sethtucker88@gmail.com>
2016-10-19 15:59:43 -0500

    Code clean up and additional comments

--- easy_challenges/sum_of_primes/sumPrimes.cpp
+++ easy_challenges/sum_of_primes/sumPrimes.cpp
@@ -1,6 +1,10 @@
 /*
 Program: sumPrimes.cpp
+Author: Seth Tucker
 Date: 8/31/2016
+Last Modified: 10/19/2016
+
+Program sums up the first 1000 primes.
 */
 
 #include <iostream>
@@ -8,30 +12,47 @@ Date: 8/31/2016
 using std::cout;
 using std::endl;
 
-bool primeCheck(int current) {
-	for (int i = 1; i < current; i++) {
+//Checks a given number to see if its prime or not
+bool primeCheck(long &current) {
+	/*
+	Takes the current number and tries to evenly
+	devide it by every number less than it and greater
+	than 1. If nothing between 1 and current - 1 evenly
+	divides it than current is prime.
+	*/
+	for (auto i = 1; i < current; i++) {
 		if (current % i == 0 && i != 1) { return false; }
 	}
-}
 
-int main() {
-	int sum = 0;
-	int numPrimes = 1000;
-	int count = 0;  //Count of checked primes
-	int current = 2;  //Current prime being checked
+}  //End primeCheck()
 
+//Function to sum primes
+void sum(long &numPrimes){
+	long sum = 0;  //Stores sum of primes
+	long count = 0;  //How many primes have been found
+	long current = 2;  //Start at first prime number
+	
+	//Loops until the number of found primes = the upper bound
 	while (count < numPrimes) {
-		if (primeCheck(current) != false) {
+		if (primeCheck(current) != false){
 			sum += current;
 			count++;
 		}
 		current++;
-
 	}
-
+	
+	//Display the sum after upper bound is reached
 	cout << sum << endl;
+	
+}  //End sum()
 
-	//system("pause");
+int main() {
+	//How many primes to sum up, i.e. the upper bound
+	long numPrimes = 1000;
+	
+	//Function call
+	sum(numPrimes);
+
 	return 0;
 
-}
+}  //End main()
--- moderate_challenges/longest_lines/LongestLines.cpp
+++ moderate_challenges/longest_lines/LongestLines.cpp
@@ -2,6 +2,14 @@
 Program: LongestLines.cpp
 Date: 9/27/2016
 Last Modified: 10/19/2016
+
+Program receives lines of text from a file and finds the
+longest ones. The first line of the file contains a number 
+which determines how many of the longest lines to display 
+starting with the longest. Trailing white space is also 
+removed so:
+	"A short line        " is not longer than
+	"A longer line"
 */
 
 #include <iostream>
@@ -19,26 +27,23 @@ using std::vector;
 using std::istringstream;
 using std::sort;
 
-int getNumLines(string fileName) {
-	ifstream source;
-	source.open(fileName);
-	int numLines;
-	string line;
-	getline(source, line);
-	istringstream iss(line);
-	iss >> numLines;
-	return numLines;
-}
-
-vector<string> getLines(string fileName) {
-	ifstream source; 
-	source.open(fileName);
+//Pulls each line from the text file and stores in abort
+//vectore of strings
+vector<string> getLines(string &fileName, long &numLines) {
+	ifstream source(fileName);
 
 	vector<string> inputs;
 
 	if (source) {
 		string line;
 		getline(source, line);
+
+		//Pulls the first line which is a number used to
+		//determine how many longets lines to display
+		istringstream iss(line);
+		iss >> numLines;
+
+		//Puts remaining lines into a vector of strings
 		while (getline(source, line)) {
 			inputs.push_back(line);
 		}
@@ -47,46 +52,60 @@ vector<string> getLines(string fileName) {
 
 }  //End getLines()
 
+//Sorts lines based on length starting with the longest
 void sortLines(vector<string> &inputs) {
-	auto range = inputs.size();
+	long range = inputs.size();
+
+	//Go through every line...
 	for (auto i = 0; i < range; i++) {
+		//and compare it to every other line
 		for (auto j = 0; j < range; j++) {
+			//If we find a line thats longer than our currently selected one...
+			//swap them
 			if (inputs.at(j).length() < inputs.at(i).length()) {
-				string temp = inputs.at(i);
-				inputs.at(i) = inputs.at(j);
-				inputs.at(j) = temp;
+				string temp = inputs.at(i);  //Store current line
+				inputs.at(i) = inputs.at(j);  //Store longer line at current's index
+				inputs.at(j) = temp;  //Store current at longer lines index
 			}
 		}
 	}
 
 }  //End sortLines()
 
+//Removes trailing whitespace from given line of text
 void trimLines(vector<string> &inputs) {
-	string whitespace = " \t";
+	string whitespace = "  ";
 	for (auto i = 0; i < inputs.size(); i++) {
+		//Select line to process
 		string line = inputs.at(i);
+		//Find begining of line, i.e. first non-whitespace character
 		auto strBegin = line.find_first_not_of(whitespace);
+		//Find end of line, where there is more than one whitespace
 		auto strEnd = line.find_last_not_of(whitespace);
+		//Calculate how many characters are between begining and end
 		auto strRange = strEnd - strBegin + 1;
 		
+		//Overwrite the line at the index with a substring with trailing
+		//whitespace removed
 		inputs.at(i) = line.substr(strBegin, strRange);
 	}
 
 }  //End trimLines()
 
-void displayLines(vector<string> inputs, int numLines) {
+//Displays n longest lines
+void displayLines(vector<string> &inputs, long &numLines) {
 	for (auto i = 0; i < numLines; i++) cout << inputs.at(i) << endl;
 
 }  //End displayLines()
 
 int main(int argc, char *argv[]) {
 	string fileName(argv[1]);
-	
+		
 	//Get number of longest lines to display
-	int numLines = getNumLines(fileName);
+	long numLines;// = getNumLines(fileName);
 
 	//Store lines from input file in a vector
-	vector<string> inputs = getLines(fileName);
+	vector<string> inputs = getLines(fileName, numLines);
 
 	//Trim trailing whitespace from strings
 	trimLines(inputs);
